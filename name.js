0d155eda7396e7d1ea5f0f420231d05260506bad
CaRTipe <blairmbugua868@gmail.com>
2025-05-02 11:10:07 +0300

    commit

---
+++ name.js
@@ -0,0 +1,20 @@
+var button = document.querySelector("button")
+var name = document.getElementById("name")
+var place = document.getElementById("place")
+var month = document.getElementById("month")
+var year = document.getElementById("year")
+var email = document.getElementById("email")
+var password = document.getElementById("password")
+var telephone = document.getElementById("telephone")
+var male = document.getElementById("male")
+var female = document.getElementById("female")
+
+function signup() {
+    var name = document.getElementById("name").value
+    var place = document.getElementById("place").value
+    var month = document.getElementById("month").value
+    var year = document.getElementById("year").value
+    var email = document.getElementById("email").value
+    var password = document.getElementById("password").value
+    var telephone = document.getElementById("telephone").value
+}
