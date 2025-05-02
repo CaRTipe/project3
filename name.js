febb7543aaff771c04f6dc334ce119ac9421d97a
CaRTipe <blairmbugua868@gmail.com>
2025-05-02 11:15:47 +0300

    commit

--- name.js
+++ name.js
@@ -1,20 +1,28 @@
-var button = document.querySelector("button")
-var name = document.getElementById("name")
-var place = document.getElementById("place")
-var month = document.getElementById("month")
-var year = document.getElementById("year")
-var email = document.getElementById("email")
-var password = document.getElementById("password")
-var telephone = document.getElementById("telephone")
-var male = document.getElementById("male")
-var female = document.getElementById("female")
+var button = document.querySelector("button");
+var name = document.getElementById("name");
+var place = document.getElementById("place");
+var month = document.getElementById("month");
+var year = document.getElementById("year");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var confirmpassword = document.getElementById("confirmpassword");
+var telephone = document.getElementById("telephone");
+var male = document.getElementById("male");
+var female = document.getElementById("female");
 
 function signup() {
-    var name = document.getElementById("name").value
-    var place = document.getElementById("place").value
-    var month = document.getElementById("month").value
-    var year = document.getElementById("year").value
-    var email = document.getElementById("email").value
-    var password = document.getElementById("password").value
-    var telephone = document.getElementById("telephone").value
+  var name = document.getElementById("name").value;
+  var place = document.getElementById("place").value;
+  var month = document.getElementById("month").value;
+  var year = document.getElementById("year").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var telephone = document.getElementById("telephone").value;
+  function validatePassword() {
+    var password = document.getElementById("password").value;
+    var confirmpassword = document.getElementById("confirmpassword").value;
+    if (password != confirmpassword) {
+      alert("Passwords do not match");
+    }
+  }
 }
